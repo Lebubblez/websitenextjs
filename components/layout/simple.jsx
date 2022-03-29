6a6df041cc3b392a5de62d3ecfbc24749685274f
Leda <leda.erlandson@centauricorp.com>
2022-03-28 15:55:51 -1000

    Added top title, and lowered the nav bar below it

--- components/layout/simple.jsx
+++ components/layout/simple.jsx
@@ -1,15 +1,23 @@
 import Navbar from '../navbar/navbar'
 import React from 'react'
+import Home from '../../pages'
 
 export default function SimpleLayout(props) {
   return (
     <>
-      <Navbar />
       <main role="main">
-        {props.preContainer && props.preContainer}
-        <div className="album py-5 bg-light">
+        <div className="album py-5">
+          <div className="container">
+          <section className="jumbotron text-center">
+          <h1>Leda Erlandson</h1>
+          </section>
+          </div>
+          <Navbar />
+          {props.preContainer && props.preContainer}
+          <div className="album py-5">
           <div className="container">
             {props.children}
+          </div>
           </div>
         </div>
       </main>
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,6 @@ import SimpleLayout from '../components/layout/simple'
 export default function Home(initialData) {
   return (
     <SimpleLayout>
-      <section className="jumbotron text-center">
-        <div className="container">
-          <h1>Leda Erlandson</h1>
-          <p className="lead text-muted">
-            Portfolio
-          </p>
-        </div>
-      </section>
-
       <div className="row">
         <h1>Sample</h1>
       </div>
